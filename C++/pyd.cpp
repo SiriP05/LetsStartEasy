3346e4b4d9cd807abfebe3ac0e2de6d23a677eec
SiriP05 <73424209+SiriP05@users.noreply.github.com>
2020-10-31 00:03:12 +0530

    Create pyd.cpp

---
+++ C++/pyd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int spaces, rows;
+    cout <<"Enter number of rows: ";
+    cin >> rows;
+    for(int i = 1, k = 0; i <= rows; ++i, k = 0)
+    {
+        for(space = 1; space <= rows-i; ++space)
+           cout <<"  ";
+        while(k != 2*i-1)
+        {
+          cout << "* ";
+          ++k;
+        }
+        cout << endl;
+    }    
+    return 0;
+}
